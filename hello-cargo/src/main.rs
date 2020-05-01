d24d65363994073b3e5a08d52b9683cf59049727
ilovelili <route666@live.cn>
2020-05-01 22:18:27 +0900

    add memo on ownership and slice

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("Hello, world!");
+    print(&s);
+    print(&s);
+
+    let mut k = String::from("Hello");
+    mutable_print(&mut k);
+    mutable_print(&mut k);
+
+    print_take_ownership(s);
+    // print_take_ownership(s); // can't call s here since s has been moved (it's not a copy)
+}
+
+fn print(s: &String) {
+    println!("{}", s);
+}
+
+fn mutable_print(s: &mut String) {
+    s.push_str(", ade");
+    println!("{}", s);
+}
+
+fn print_take_ownership(s: String) {
+    println!("{}", s);
 }

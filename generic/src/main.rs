6b37deb31f0ca45516ee6b8d9b53a3e19f9e5d51
ilovelili <route666@live.cn>
2020-05-03 16:03:51 +0900

    generic, trait, lifetime

---
+++ generic/src/main.rs
@@ -0,0 +1,69 @@
+use std::fmt::Display;
+
+fn main() {
+    let a;
+    {
+        let x = 5;
+        a = x;
+        // a = &x // x does not live long enough error
+    }
+    println!("{}", a);
+}
+
+fn longest<'a>(a: &'a str, b: &'a str) -> &'a str {
+    if a.len() > b.len() {
+        a
+    } else {
+        b
+    }
+}
+
+// To call this code with only those types that implement the Copy trait, we
+// can add Copy to the trait bounds of T !
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+// only implements the cmp_display method if its inner type
+// T implements the PartialOrd trait that enables comparison and
+// the Display trait that enables printing.
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
